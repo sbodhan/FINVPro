073726854699e9b0bf399c9b866955cecd98a571
Srinivas Bodhanampati <sbodhan@Srinivass-MacBook-Pro.local>
2016-07-12 15:24:35 -0400

    Initial Version

---
+++ FINVPro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FINVPro
+//
+//  Created by Srinivas Bodhanampati on 7/12/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
